6686eb21501890b81104362a645e694d9c347717
Collin Sparks <cklsparks@gmail.com>
2021-07-29 13:30:00 -0500

    fastapi working in its most basic form

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
+
 from fastapi import FastAPI
 import json
 from typing import Union, List, Dict
+
 
 
 app: FastAPI = FastAPI()
 
 
 @app.get("/")
-async def root() -> None:
-    pass
+async def root() -> Dict[str,str]:
+    return {"hello": "world"}
 
 @app.get("/send_email")
 async def send_email() -> Dict[str, str]:
